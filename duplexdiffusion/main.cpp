0fac86d3f725847ec44db5b15db82a1e575936ad
Danilo Naiff <danilonaiff1992@poli.ufrj.br>
2022-03-10 18:11:49 -0300

    More realistic data

--- duplexdiffusion/main.cpp
+++ duplexdiffusion/main.cpp
@@ -11,18 +11,20 @@ int main()
     duplexsolver::SolParams solparams;
     //solparams.nkernel = 10
     parameters.R = 5;
-    parameters.vol_fraction = 0.4; //0.1
+    parameters.vol_fraction = 0.4; //0.4
     parameters.L = 100;
-    solparams.maxwindow = 1000;
+    solparams.maxwindow = 1000*5;//1000
     duplexsolver::Solver solver(parameters, solparams);
     solver.prepare_linear_system();
     int nsteps = 100000;
+    nsteps *= 5;
+    solparams.timestep /= 5;
 //
     timer::Timer t;
     std::ofstream file;
     std::ofstream filestep;
     t.reset();
-    file.open("../data/result_realistic");
+    file.open("../data/result_realistic_detailed");
     for(int i = 0; i < nsteps; i++){
         //solver.step();
         file << solver.step().transpose() << std::endl;
